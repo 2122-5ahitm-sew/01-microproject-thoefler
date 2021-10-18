4b10dc9e0ad5439f50ea9395c70555b0858a1f1e
Tobert <tobiashoefl@gmail.com>
2021-10-18 20:29:53 +0200

    created new version of cld

---
+++ src/main/java/at/htl/Entity/Artist.java
@@ -0,0 +1,15 @@
+package main.java.at.htl.Entity;
+
+import java.util.List;
+
+public class Artist {
+
+    private String artistName;
+    private String firstName;
+    private String lastName;
+    private List<Singles> singlesList;
+    private int monthlyListeners;
+
+
+
+}
---
+++ src/main/java/at/htl/Entity/Singles.java
@@ -0,0 +1,4 @@
+package main.java.at.htl.Entity;
+
+public class Singles {
+}
